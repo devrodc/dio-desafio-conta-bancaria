1cdc2a92766de61d62e11d893b212dbbdf60ea81
rodrigocesar <rodc021@gmail.com>
2024-05-07 20:11:05 -0300

    Adicionando os dados no sistema e configurando mensagem final ao usuário

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -11,7 +11,22 @@ public class ContaTerminal {
 		String nomeCliente;
 		double saldo;
 		
+		System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = dados.nextInt();
 
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = dados.next();
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = dados.next();
+
+        System.out.println("Por favor digite seu saldo: ");
+        saldo = dados.nextDouble();
+        
+        System.out.println(
+                "Olá, " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", "
+                        + "conta " + numeroConta + " e seu saldo é R$" + saldo + " reais e já está disponível para saque");
+		
 	}
 
 }
